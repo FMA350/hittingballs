6e53d2bad28e24130b47178b10b415d885e8ee85
Francesco Maria Moneta (BFS EUROPE) <FR20070493@wipro.com>
2019-03-14 14:53:25 +0000

    first commit of the ping_pong project

---
+++ Ping-pong-server/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const qs = require('querystring');
+
+const hostname = '127.0.0.1';
+const port = 100;
+
+const server = http.createServer((req, res) => {
+    var body = '';
+    // console.log(req);
+    // console.log('New request from' + req.ip);
+    if(req.method == 'POST'){
+        req.on('data', function (data){
+            body += data;
+            if (body.length > 1e6) {
+                req.connection.destroy();
+            }
+            req.on('end', function (){
+            console.log(body);
+            res.end("I bought it")
+            });
+        });
+    }
+    if(req.method == 'GET'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, World!\n');
+    }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
